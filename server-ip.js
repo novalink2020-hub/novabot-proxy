fa0b672756e98f4846e578bf06e539440470479b
novalink2020-hub <novalink2020@gmail.com>
2025-11-14 19:04:14 +0200

    Delete server-ip.js

--- server-ip.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ server_ip: ip }));
-}
