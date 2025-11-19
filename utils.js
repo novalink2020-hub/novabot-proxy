2d60a786a688b5584a75cb1136c7b6a1c1cc9e5e
novalink2020-hub <novalink2020@gmail.com>
2025-11-19 19:37:24 +0200

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-// =======================================================
-// utils.js – General Helpers
-// =======================================================
-
-function sleep(ms) {
-  return new Promise(res => setTimeout(res, ms));
-}
-
-module.exports = { sleep };
