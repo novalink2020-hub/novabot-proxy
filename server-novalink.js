eff4721f49216c2045dc60f911028262ef311cce
novalink2020-hub <novalink2020@gmail.com>
2025-11-25 23:23:52 +0200

    Update server-novalink.js

--- server-novalink.js
+++ server-novalink.js
@@ -7,7 +7,7 @@ import http from "http";
 
 // استدعاء وحدات الذكاء
 import { detectNovaIntent } from "./novaIntentDetector.js";
-import {   normalizeItem,   findBestMatch,   buildStrongMatchReply,   buildMidMatchTemplateReply,   buildGeminiPrompt } from "./novaBrainSystem.js";
+import { novaBrainSystem } from "./novaBrainSystem.js";
 
 // -------------------------------
 //  إعدادات السيرفر
