ccd2aa23177da3e867e4830bd7b37e952b977d87
novalink2020-hub <novalink2020@gmail.com>
2025-12-02 23:11:01 +0200

    Update novaIntentDetector.js

--- novaIntentDetector.js
+++ novaIntentDetector.js
@@ -1,1 +1,13 @@
+export function detectNovaIntent(input) {
+  return {
+    originalIntentId: "ai_business",
+    effectiveIntentId: "ai_business",
+    sessionTier: "strong_ai",
+    hasAIMomentum: true,
+    allowGemini: true,
+    language: "ar",
+    dialectHint: "neutral"
+  };
+}
 
+export default detectNovaIntent;
